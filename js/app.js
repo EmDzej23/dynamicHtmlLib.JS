7db07047862380a45d7ad6a655162487618e461d
EmDzej23 <markojereminov@hotmail.com>
2017-10-19 17:08:47 +0200

    Small UI change

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
     AbstractDHElement.prototype.appendData("#container", DHElement("div","col-md-9","fifth","").html);
     AbstractDHElement.prototype.appendData("#fifth", DHElement("div","col-md-12","fifth_2","").html);
     DHElement("button","btn btn-primary col-md-8","btngd",'GET DATA FROM URL',["onclick:getData()"]).appendTo("#fifth_1");     
-    DHEmptyElement("input","col-md-10","url_input","https://jsonplaceholder.typicode.com/users/","text",[],"width:450px;margin:5px;").appendTo("#fifth_1");
+    DHEmptyElement("input","col-md-8","url_input","https://jsonplaceholder.typicode.com/users/","text",[],"margin:5px;").appendTo("#fifth_1");
     DHElement("button","btn btn-primary col-md-8","btngd",'GET DATA FROM JSON',["onclick:getDataFromJson()"]).appendTo("#fifth_1");     
-    DHElement("textarea","col-md-12","json_input","{\"KEY\":\"VALUE\"}",[],"width:450px;margin:5px;height:10%").appendTo("#fifth_1");     
+    DHElement("textarea","col-md-8","json_input","{\"KEY\":\"VALUE\"}",[],"margin:5px;height:10%").appendTo("#fifth_1");     
     DHElement("div","col-md-12","json_tree","").appendTo("#fifth_1"); 
 });
 function prepareForWriting(data) {
